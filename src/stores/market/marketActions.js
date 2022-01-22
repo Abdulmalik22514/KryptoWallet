9eb2404f139df47c6c01de77b84405b5660f7171
Abdulmalik Ibrahim <ibrahimmalik095@gmail.com>
2022-01-22 06:56:53 +0100

    feat: Complete getCoin actions

--- src/stores/market/marketActions.js
+++ src/stores/market/marketActions.js
@@ -91,3 +91,60 @@ export function getHoldings(
       .catch(error => dispatch(getHoldingsFailure(error)));
   };
 }
+
+// coins
+
+export const getCoinMarketBegin = () => {
+  return {
+    type: COIN_MARKET_BEGIN,
+  };
+};
+
+export const getCoinMarketSuccess = coins => {
+  return {
+    type: COIN_MARKET_SUCCESS,
+    payload: coins,
+  };
+};
+
+export const getCoinMarketFailure = error => {
+  return {
+    type: COIN_MARKET_FAILURE,
+    payload: error,
+  };
+};
+
+export function getCoinMarket(
+  currency = 'usd',
+  sparkline = true,
+  orderBy = 'market_cap_desc',
+  priceChangePerc = '7d',
+  perPage = 10,
+  page = 1,
+) {
+  return dispatch => {
+    dispatch(getCoinMarketBegin());
+
+    let apiUrl = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=${orderBy}&per_page=${perPage}&page=${page}&sparkline=${sparkline}&price_change_percentage=${priceChangePerc}
+    `;
+
+    return axios({
+      url: apiUrl,
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+      },
+    })
+      .then(response => {
+        console.log(response.data);
+        if (response.state == 200) {
+          dispatch(getCoinMarketSuccess(response.data));
+        } else {
+          dispatch(getCoinMarketFailure(response.data));
+        }
+      })
+      .catch(error => {
+        dispatch(getCoinMarketFailure(error));
+      });
+  };
+}
